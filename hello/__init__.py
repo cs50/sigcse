0ed587188cbea2cd63d3292e8a45ccadd881ee76
Chad Sharp <chad@cs50.harvard.edu>
2021-03-14 10:14:14 -0400

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -4,7 +4,7 @@ import check50.c
 @check50.check()
 def compiles():
     """hello compiles"""
-    check50.c.compile("hello.c")
+    check50.run("clang hello.c -o hello").exit(0)
 
 @check50.check(compiles)
 def prints_hello():
